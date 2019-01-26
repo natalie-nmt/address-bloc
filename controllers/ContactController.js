c0a90eca6315ed7894a478b42304deb26a86d350
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-01-25 21:44:02 -0500

    Added email column

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -20,13 +20,21 @@ module.exports = class ContactController {
           message: "Contact's phone number - ",
           validate(val){
             return val !== "";
-          }
+            }
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Contact's email address - ",
+            validate(val){
+              return val !== "";
+            }
         }
       ];
   }
 
-  addContact(name, phone){
-    return Contact.create({name, phone})
+  addContact(name, phone, email){
+    return Contact.create({name, phone, email})
   }
 
 }
