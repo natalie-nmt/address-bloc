77ef4ffcae53d37ec669bbdae3a39b10c245f5b8
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-01-26 14:23:30 -0500

    Removed test remindMe function

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -4,15 +4,4 @@ describe("MenuController", () => {
     beforeEach(() => {
         this.menu = new MenuController();
       });
-
-    
-
-    describe("#remindMe()", () => {
-
-        it("should return exactly 'Learning is a life-long pursuit.'", () => {
-          expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit.');
-        });
-  
-      });
-
   });
